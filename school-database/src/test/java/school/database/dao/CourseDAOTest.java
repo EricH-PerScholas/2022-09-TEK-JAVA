cf821df4cfee540b34b1b4d4c5876b92cbad601d
Anjaom <eric@webworkbench.net>
2022-10-31 13:14:03 -0600

    added additional test cases and now a new asignemnet to valudate an email address

--- school-database/src/test/java/school/database/dao/CourseDAOTest.java
+++ school-database/src/test/java/school/database/dao/CourseDAOTest.java
@@ -56,5 +56,37 @@ public class CourseDAOTest {
 		Assertions.assertEquals(expected.getName(), actual.getName());
 		Assertions.assertEquals(expected.getDeptId(), actual.getDeptId());
 	}
+	
+	@Test
+	public void testGetCourseStartsWithName() {
+		// given
+		Course expected1 = new Course();
+		
+		expected1.setId(41);
+		expected1.setName("History");
+		expected1.setDeptId(3);
+		
+		Course expected2 = new Course();
+		
+		expected2.setId(42);
+		expected2.setName("History of Art, Architecture & Design");
+		expected2.setDeptId(1);
+		
+		// when 
+		List<Course> actualList = courseDAO.findByCourseNameStartsWith("History");
+		
+		// then
+		Assertions.assertTrue(actualList.size() == 2);
+		
+		Course actual1 = actualList.get(0);
+		Assertions.assertEquals(expected1, actual1);
+		Assertions.assertEquals(expected1.getName(), actual1.getName());
+		Assertions.assertEquals(expected1.getDeptId(), actual1.getDeptId());
+
+		Course actual2 = actualList.get(1);
+		Assertions.assertEquals(expected2, actual2);
+		Assertions.assertEquals(expected2.getName(), actual2.getName());
+		Assertions.assertEquals(expected2.getDeptId(), actual2.getDeptId());
+	}
 
 }
