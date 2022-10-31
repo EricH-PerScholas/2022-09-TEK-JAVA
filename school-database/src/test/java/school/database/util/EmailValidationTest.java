ea563d2577d77821bdd138ce6560418f17e9d400
Anjaom <eric@webworkbench.net>
2022-10-31 14:30:37 -0600

    added parameterized test case for email

--- school-database/src/test/java/school/database/util/EmailValidationTest.java
+++ school-database/src/test/java/school/database/util/EmailValidationTest.java
@@ -2,21 +2,16 @@ package school.database.util;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class EmailValidationTest {
 	
-	@Test
 	@ParameterizedTest
-	@ValueSource(ints = {1, 3, 5, -3, 15, Integer.MAX_VALUE})
-	public void isEmailValidTest() {
+	@ValueSource(strings = { "", "ericseriesreminder.com", "er#ic@seriesreminder.com", "eric@seriesreminder.co" })
+	public void isEmailInvalidTest(String string) {
 		EmailValidation ev = new EmailValidation();
 		
-		Assertions.assertFalse(ev.isValidEmail(null));
-		Assertions.assertFalse(ev.isValidEmail(""));
-		Assertions.assertFalse(ev.isValidEmail("ericseriesreminder.com"));
-		Assertions.assertFalse(ev.isValidEmail("er#ic@seriesreminder.com"));
-		Assertions.assertFalse(ev.isValidEmail("eric@seriesreminder.co"));
-		
-		Assertions.assertTrue(ev.isValidEmail("eric@seriesreminder.com"));
+		Assertions.assertFalse(ev.isValidEmail(string));
 	}
 }
