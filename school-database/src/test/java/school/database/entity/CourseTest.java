fb547f720f0039aaa13a9064692ba0747192af7a
Anjaom <eric@webworkbench.net>
2022-11-04 08:14:44 -0600

    html / css exercises

--- school-database/src/test/java/school/database/entity/CourseTest.java
+++ school-database/src/test/java/school/database/entity/CourseTest.java
@@ -1,9 +1,42 @@
 package school.database.entity;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import school.database.dao.CourseDAO;
 
 public class CourseTest {
 
 
+	@Test
+	public void testEquals() {
+		Course c1 = new Course();
+		c1.setId(1);
+		c1.setName("c1");
+		
+		Course c2 = new Course();
+		c2.setId(2);
+		c2.setName("c2");
+		
+		Course c3 = new Course();
+		c3.setId(1);
+		c3.setName("c1");
+		
+		Assertions.assertNotEquals(c1, c2);
+		Assertions.assertEquals(c1, c3);
+	}
+	
+	@Test
+	public void testDeptId() {
+		CourseDAO courseDao = new CourseDAO();
+		
+		Course c = courseDao.findById(1);
+		
+		int a = c.getDeptId();
+		int b = c.getDepartment().getId();
+		
+		Assertions.assertEquals(a,b);
+		
+	}
+	
 }
