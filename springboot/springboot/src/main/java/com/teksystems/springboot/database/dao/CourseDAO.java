f7956eb1aa08ce73dcacf7be346c007c68801d7a
Anjaom <eric@webworkbench.net>
2022-11-17 13:33:49 -0700

    spring boot search form

--- springboot/springboot/src/main/java/com/teksystems/springboot/database/dao/CourseDAO.java
+++ springboot/springboot/src/main/java/com/teksystems/springboot/database/dao/CourseDAO.java
@@ -14,6 +14,7 @@ public interface CourseDAO extends JpaRepository<Course, Long> {
 	@Query("SELECT c FROM Course c where c.name = :name")
 	public List<Course> findByCourseName(String name);
 	
+	// select * from course where lower(name) like lower('%name%') order by name desc
 	public List<Course> findByNameContainingIgnoreCaseOrderByNameDesc(String name);
 	
 	public Course findByNameAndId(String name, Integer id);
